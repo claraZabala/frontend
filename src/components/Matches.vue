756b2957224c6ddbff0001e87d61d01430d4271a
clara <clzabala40@alumnes.ub.edu>
2022-05-03 12:57:56 +0200

    canvis

---
+++ src/components/Matches.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1> {{ message }} </h1>
+    <button class="btn btn-success btn-lg" @click="buyTicket"> Buy ticket </button>
+    <h4> Total tickets bought: {{ tickets_bought }} </h4>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CaCa',
+  data () {
+    return {
+      message: 'My first component',
+      tickets_bought: 0
+    }
+  },
+  methods: {
+    buyTicket () {
+      this.tickets_bought += 1
+    }
+  }
+}
+
+</script>
